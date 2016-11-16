db55648dcf0b3bfe9f7612070f06698116186938
= <admin@ryancollins.io>
2016-11-16 01:52:09 -0500

    Feat: Update main program with Edit and Score msgs

--- score-keeper/Main.elm
+++ score-keeper/Main.elm
@@ -3,6 +3,7 @@ import Html exposing (..)
 import Html.App as App exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import String
 
 type alias Model =
   {
@@ -54,8 +55,97 @@ update msg model =
       Debug.log "Input model updated"
       { model | name = name }
 
+    Cancel ->
+      { model | name = "", playerId = Nothing }
+
+    Save ->
+      if (String.isEmpty model.name) then
+        model
+      else
+        save model
+
+    Score player points ->
+      score model player points
+
+    Edit player ->
+      { model | name = player.name, playerId = Just player.id }
+
+
     _ ->
       model
+
+score : Model -> Player -> Int -> Model
+score model scorer points =
+  let
+    newPlayers =
+      List.map
+        (\player ->
+          if player.id == scorer.id then
+            { player
+            | points = player.points + points
+            }
+          else
+            player
+        )
+        model.players
+    play =
+      Play (List.length model.plays) scorer.id scorer.name points
+  in
+    { model | players = newPlayers, plays = play :: model.plays }
+
+
+save : Model -> Model
+save model =
+  case model.playerId of
+    Just id ->
+      edit model id
+    Nothing ->
+      add model
+
+add : Model -> Model
+add model =
+  let
+    player =
+      Player (List.length model.players) model.name 0
+
+    newPlayers =
+      player :: model.players
+
+  in
+    { model
+      | players = newPlayers
+      , name = ""
+    }
+
+
+edit: Model -> Int -> Model
+edit model id =
+  let
+    newPlayers =
+      List.map
+        (\player ->
+          if player.id == id then
+            { player | name = model.name }
+          else
+            player
+        )
+        model.players
+    newPlays =
+      List.map
+        (\play ->
+          if play.playerId == id then
+            { play | name = model.name }
+          else
+            play
+        )
+        model.plays
+  in
+    { model
+    | players = newPlayers
+    , plays = newPlays
+    , name = ""
+    , playerId = Nothing
+    }
 
 -- view
 
@@ -63,8 +153,67 @@ view : Model -> Html Msg
 view model =
   div [ class "scoreboard" ]
       [ h1 [] [ text "Score Keeper"]
+      , playerSection model
       , playerForm model
       , p [] [ text (toString model)]
+      ]
+
+playerSection : Model -> Html Msg
+playerSection model =
+  div []
+    [ playerListHeader
+    , playerList model
+    , pointTotal model
+    ]
+
+playerListHeader : Html Msg
+playerListHeader =
+  header []
+  [ div [] [ text "Name" ]
+  , div [] [ text "Points" ]
+  ]
+
+playerList : Model -> Html Msg
+playerList model =
+  model.players
+  |> List.sortBy .name
+  |> List.map player
+  |> ul []
+
+player : Player -> Html Msg
+player player =
+  li []
+  [ i
+    [ class "edit"
+    , onClick (Edit player)
+    ]
+    []
+  , div []
+    [ text player.name]
+  , button
+    [type' "button"
+    , onClick (Score player 2)
+    ]
+    [ text "2pt"]
+  , button
+    [type' "button"
+    , onClick (Score player 3)
+    ]
+    [ text "3pt"]
+    , div []
+      [ text (toString player.points)]
+  ]
+
+pointTotal : Model -> Html Msg
+pointTotal model =
+  let
+    total =
+      List.map .points model.plays
+        |> List.sum
+  in
+    footer []
+      [ div [] [ text "Total:"]
+      , div [] [ text (toString total) ]
       ]
 
 playerForm : Model -> Html Msg
